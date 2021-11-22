2670450581b795a9b338adf929e2c2c03c3c708a
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-11-22 13:34:34 -0300

    second commit and API finished

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f7r(dm1i^%cw9m9#^2_ou4%&vpk-)!^3z4!@0vac@r*!bie1fb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'books',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
